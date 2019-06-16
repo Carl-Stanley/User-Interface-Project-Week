be56436dea5c121cabdb769fbd8c39cc5898a4e5
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-06-16 13:15:41 -0700

    Commit

---
+++ js/IndexContent.js
@@ -0,0 +1,39 @@
+const siteContent = {
+
+    "a" : {
+
+    },
+    
+    "addresses" : {
+
+
+    },
+    
+    "footer" : {
+
+    },
+
+    "H" : {
+
+        
+
+    },
+    
+    "header" : {
+
+    },
+
+    "menu" : {
+
+        "1" : "Home",
+        "2" : "Services",
+        "3" : "Contact",
+
+    },    
+
+    "p" : {
+
+
+    }    
+
+};
