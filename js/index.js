d6ed99a46a667e081d185d4764b63020717969ce
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-06-17 09:19:04 -0700

    Commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,83 @@
 // JS goes here
+// Content for Index Page
+const siteContentIndex = {
+
+    "a" : {
+
+    },
+    
+    "addresses" : {
+
+
+    },
+    
+    "footer" : {
+
+    },
+
+    "H" : {
+
+        "1" : "Integrity",
+        "2" : "Excellence",
+        "3" : "Progress.",
+        "4" : "Smith & Jones Architects"
+
+    },
+    
+    "header" : {
+
+    },
+
+    "menu" : {
+
+        "1" : "Home",
+        "2" : "Services",
+        "3" : "Contact",
+
+    },    
+
+    "p" : {
+
+
+    }    
+
+};
+
+// Content for Services Page
+const siteContentServices = {
+
+    "a" : {
+
+    },
+    
+    "addresses" : {
+
+
+    },
+    
+    "footer" : {
+
+    },
+
+    "H" : {
+
+        
+
+    },
+    
+    "header" : {
+
+    },
+
+    "menu" : {
+
+        
+
+    },    
+
+    "p" : {
+
+
+    }    
+
+};
